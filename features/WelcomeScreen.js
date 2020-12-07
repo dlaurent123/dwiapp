2fba25492643897f664d6223239a83b7a032b248
dannyl <danilovlaurent@pursuit.org>
2020-12-06 21:18:22 -0500

    create buttons on welcome page

--- features/WelcomeScreen.js
+++ features/WelcomeScreen.js
@@ -9,6 +9,7 @@ import {
   Button,
   Alert,
   ImageBackground,
+  SafeAreaView,
 } from "react-native";
 
 const WelcomeScreen = () => {
@@ -16,8 +17,31 @@ const WelcomeScreen = () => {
     <ImageBackground
       style={{ height: "100%", width: "100%" }}
       source={require("../assets/background.jpg")}
-    ></ImageBackground>
+    >
+      <View style={style.container}>
+        <SafeAreaView style={{ flex: 1 }}>
+          <Image
+            style={{ height: 20, width: 20 }}
+            source={require("../assets/logo-red.png")}
+          />
+        </SafeAreaView>
+        <View style={{ flex: 1, justifyContent: "flex-end" }}>
+          <View style={{ backgroundColor: "#fc5c65", height: 70 }}>
+            <Button title={"buut"} />
+          </View>
+          <View style={{ backgroundColor: "#4ECDC4", height: 70 }}>
+            <Button title={"buut"} />
+          </View>
+        </View>
+      </View>
+    </ImageBackground>
   );
 };
+
+const style = StyleSheet.create({
+  container: {
+    flex: 1,
+  },
+});
 
 export default WelcomeScreen;
