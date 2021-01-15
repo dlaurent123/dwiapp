cdb92495b84ac595b05ae1e7d33faf86e9308911
dannyl <danilovlaurent@pursuit.org>
2021-01-14 21:58:53 -0500

    complete form image picker

--- app/components/Forms/FormImagePicker.js
+++ app/components/Forms/FormImagePicker.js
@@ -1,14 +1,34 @@
 import React from "react";
-import ImageInputList from "../ImageInputList";
 import ErrorMessage from "./ErrorMessage";
+import ImageInputList from "../ImageInputList";
 import { useFormikContext } from "formik";
 
-function FormImagePicker(props) {
+function FormImagePicker({ name }) {
   const { touched, errors, setFieldValue, values } = useFormikContext();
+  const imageUris = values[name];
+
+  const handleAdd = (uri) => {
+    setFieldValue(name, [...imageUris, uri]);
+  };
+  const handleRemove = (uri) => {
+    const newImageUris = () =>
+      imageUris.filter((el) => {
+        if (el !== uri) {
+          return el;
+        }
+      });
+
+    setFieldValue(name, newImageUris);
+  };
+
   return (
     <>
-      <ImageInputList imageUris={} />
-      <ErrorMessage />
+      <ImageInputList
+        imageUris={imageUris}
+        onAddImage={handleAdd}
+        onRemoveImage={handleRemove}
+      />
+      <ErrorMessage error={errors[name]} visible={touched[name]} />
     </>
   );
 }
