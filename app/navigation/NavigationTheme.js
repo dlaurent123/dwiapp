b5ce0bee49434ccd8de37ba7945988221c0c8a2e
dannyl <danilovlaurent@pursuit.org>
2021-01-17 23:38:20 -0500

    overide default colors in react navigation with colors from project color theme

--- app/navigation/NavigationTheme.js
+++ app/navigation/NavigationTheme.js
@@ -1,1 +1,13 @@
 import { DefaultTheme } from "@react-navigation/native";
+import colors from "../config/colors";
+
+const myTheme = {
+  ...DefaultTheme,
+  color: {
+    ...DefaultTheme.colors,
+    primary: colors.primary,
+    background: colors.white,
+  },
+};
+
+export default myTheme;
