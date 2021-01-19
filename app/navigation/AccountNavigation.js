d3fb06a65250cdb1818bd3c6b6973b81cd84dc28
dannyl <danilovlaurent@pursuit.org>
2021-01-18 22:53:46 -0500

    import material community icons

--- app/navigation/AccountNavigation.js
+++ app/navigation/AccountNavigation.js
@@ -2,6 +2,7 @@ import React from "react";
 import { createStackNavigator } from "@react-navigation/stack";
 import MyaccountScreen from "../screens/MyaccountScreen";
 import Messaging from "../screens/Messaging";
+import { MaterialCommunityIcons } from "@expo/vector-icons";
 
 const Stack = createStackNavigator();
 
