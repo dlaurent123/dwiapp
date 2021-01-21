beaaf90ad932907171fbed3a5efb3e7ef829dd1d
dannyl <danilovlaurent@pursuit.org>
2021-01-20 23:03:23 -0500

    create useApi hook

---
+++ app/hooks/useAPI.js
@@ -0,0 +1,21 @@
+import React, { useState } from "react";
+
+const useAPI = (apiFunction) => {
+  const [data, setData] = useState();
+  const [hasError, setError] = useState();
+  const [isLoading, setIsLoading] = useState(true);
+
+  const request = async () => {
+    const res = await apiFunction();
+    setIsLoading(false);
+
+    if (!res.ok) return setError(true);
+
+    setError(false);
+    setData(res.data);
+  };
+
+  return { request, data, hasError, isLoading };
+};
+
+export default useAPI;
