09183a8b86b8e2859e06419d9d2bb3a2ac5f154e
dannyl <danilovlaurent@pursuit.org>
2021-01-21 01:13:42 -0500

    add objet to image source for listing details screen

--- app/screens/UploadScreen.js
+++ app/screens/UploadScreen.js
@@ -2,12 +2,17 @@ import React from "react";
 import { View, StyleSheet, Modal, Text } from "react-native";
 import Progress from "react-native-progress/Bar";
 import colors from "../config/colors";
+import LottieView from "lottie-react-native";
 
 function UploadScreen({ progress = 0, visible = false }) {
   return (
     <Modal visible={visible}>
       <View style={styles.container}>
-        <Progress color={colors.primary} progress={progress} width={200} />
+        {progress > 1 ? (
+          <Progress color={colors.primary} progress={progress} width={200} />
+        ) : (
+          <LottieView source={require("../assets/animations/done.json")} />
+        )}
       </View>
     </Modal>
   );
