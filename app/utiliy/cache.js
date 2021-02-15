58015829b8bfdf3635216e2066a4cd00f394dc27
dannyl <danilovlaurent@pursuit.org>
2021-02-15 18:15:04 -0500

    install dayjs to replace momentjs in the cache layer

--- app/utiliy/cache.js
+++ app/utiliy/cache.js
@@ -1,13 +1,13 @@
 import AsyncStorage from "@react-native-async-storage/async-storage";
-import moment from "moment";
+import dayjs from "dayjs";
 
 const prefix = "cache";
 const expiryInMinutes = 5;
 
 const isExpired = (item) => {
-  const now = moment(Date.now());
-  const storedTime = moment(item.timestamp);
-  return now.diff(storedTime, "minutes") > expiryInMinutes;
+  const now = dayjs();
+  const storedTime = dayjs(item.timestamp);
+  return now.diff(storedTime, "minute") > expiryInMinutes;
 };
 
 const store = async (key, value) => {
